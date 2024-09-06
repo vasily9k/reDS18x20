2b6eb526fb06d8aa28625888d8614b599dedec82
rs.pink <rs.pink@gmail.com>
2024-09-06 23:36:00 +0800

    move from kotyara12

---
+++ include/reDS18x20.h
@@ -0,0 +1,97 @@
+/* 
+   RU: Модуль для получения данных с датчиков Dallas DS18x20 из ESP32
+   EN: Module for receiving data from sensors Dallas DS18x20 from ESP32
+   --------------------------------------------------------------------------------
+   (с) 2021 Разживин Александр | Razzhivin Alexander
+   kotyara12@yandex.ru | https://kotyara12.ru | tg: @kotyara1971
+*/
+
+#ifndef __RE_DS18x20_H__
+#define __RE_DS18x20_H__
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <driver/gpio.h>
+#include "reSensor.h"
+#include "onewire.h"
+
+// Model IDs
+typedef enum {
+  MODEL_UNKNOWN  = 0x00,
+  MODEL_DS18S20  = 0x10,  // also DS1820
+  MODEL_DS18B20  = 0x28,  // also MAX31820
+  MODEL_DS1822   = 0x22,
+  MODEL_DS1825   = 0x3B,
+  MODEL_DS28EA00 = 0x42
+} DS18x20_MODEL;
+
+// Device resolution
+typedef enum {
+  DS18x20_RESOLUTION_INVALID = -1,  // Invalid resolution
+  DS18x20_RESOLUTION_9_BIT   = 9,   // 9-bit resolution, LSB bits 2,1,0 undefined
+  DS18x20_RESOLUTION_10_BIT  = 10,  // 10-bit resolution, LSB bits 1,0 undefined
+  DS18x20_RESOLUTION_11_BIT  = 11,  // 11-bit resolution, LSB bit 0 undefined
+  DS18x20_RESOLUTION_12_BIT  = 12,  // 12-bit resolution (default)
+} DS18x20_RESOLUTION;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+class DS18x20 : public rSensor {
+  public:
+    DS18x20(uint8_t eventId, 
+      gpio_num_t pin, onewire_addr_t address, int8_t index, DS18x20_RESOLUTION resolution, bool saveScratchPad,
+      const char* sensorName, const char* topicName, const bool topicLocal, 
+      const uint32_t minReadInterval = 1000, const uint16_t errorLimit = 0,
+      cb_status_changed_t cb_status = nullptr, cb_publish_data_t cb_publish = nullptr);
+    void setSensorItems(rSensorItem* itemTemperature);
+
+    sensor_status_t sensorBusReset() override;
+    sensor_status_t sensorReset() override;
+    sensor_status_t getResolution(DS18x20_RESOLUTION *resolution);
+    sensor_status_t setResolution(DS18x20_RESOLUTION resolution);
+    
+    sensor_value_t getTemperature(const bool readSensor);
+  protected:
+    sensor_status_t readRawData() override;
+
+    #if CONFIG_SENSOR_AS_JSON
+    char* jsonCustomValues() override;
+    #endif // CONFIG_SENSOR_AS_JSON
+  private:
+    typedef uint8_t scratchpad[9];
+
+  	gpio_num_t _pin = GPIO_NUM_NC;          // The GPIO pin connected to the 1-Wire bus
+    DS18x20_MODEL _model = MODEL_UNKNOWN;   // Family
+    int8_t _index = -1;                     // 1-Wire device index on bus
+    onewire_addr_t _address = ONEWIRE_NONE; // 1-Wire device ROM address (64-bit)
+    bool _parasitePower = false;            // Parasite power flag
+    bool _saveScratchPad = false;           // Values will be saved from scratchpad to EEPROM on every scratchpad write
+    DS18x20_RESOLUTION _resolution = DS18x20_RESOLUTION_INVALID; // Current resulution
+
+    bool addressSelect();
+    sensor_status_t readPowerSupply();
+
+    bool validFamily(uint8_t faminly_byte);
+    bool validAddress(uint8_t* rom_code);
+    bool readROM(uint64_t *rom_code);
+    bool scanDevices();
+
+    sensor_status_t readScratchpad(uint8_t *buffer);
+    sensor_status_t writeScratchpad(uint8_t *buffer);
+    sensor_status_t saveScratchpad();
+
+    void waitForDuration();
+    sensor_status_t waitForDeviceSignal();
+    sensor_status_t waitForConversion();
+
+    sensor_status_t startConvert();
+    sensor_status_t readTemperature(float * value);
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __RE_DS18x20_H__
---
+++ src/reDS18x20.cpp
@@ -0,0 +1,517 @@
+#include "reDS18x20.h"
+#include <math.h>
+#include "rLog.h"
+#include "rStrings.h"
+
+static const char * logTAG = "DS18x20";
+
+// OneWire commands
+#define DS18x20_READROM            0x33  // Read ROM (family code + serial number + CRC)
+#define DS18x20_TEMP_CONVERT       0x44  // Initiate a single temperature conversion
+#define DS18x20_SCRATCHPAD_WRITE   0x4E  // Write 3 bytes of data to the device scratchpad at positions 2, 3 and 4
+#define DS18x20_SCRATCHPAD_READ    0xBE  // Read 9 bytes of data (including CRC) from the device scratchpad
+#define DS18x20_SCRATCHPAD_COPY    0x48  // Copy the contents of the scratchpad to the device EEPROM
+#define DS18x20_EEPROM_RECALL      0xB8  // Restore alarm trigger values and configuration data from EEPROM to the scratchpad
+#define ds18x20_READ_PWRSUPPLY     0xB4  // Determine if a device is using parasitic power
+#define DS18x20_ALARM_SEARCH       0xEC  // Query bus for devices with an alarm condition
+
+// Scratchpad locations
+#define SP_TEMP_LSB                0
+#define SP_TEMP_MSB                1
+#define SP_HIGH_ALARM_TEMP         2
+#define SP_LOW_ALARM_TEMP          3
+#define SP_CONFIGURATION           4
+#define SP_INTERNAL_BYTE           5
+#define SP_COUNT_REMAIN            6
+#define SP_COUNT_PER_C             7
+#define SP_SCRATCHPAD_CRC          8
+
+#define CONVERSION_TIMEOUT_9_BIT   94
+#define CONVERSION_TIMEOUT_10_BIT  188
+#define CONVERSION_TIMEOUT_11_BIT  375
+#define CONVERSION_TIMEOUT_12_BIT  750
+
+static bool _check_resolution(DS18x20_RESOLUTION resolution)
+{
+  return (resolution >= DS18x20_RESOLUTION_9_BIT) && (resolution <= DS18x20_RESOLUTION_12_BIT);
+}
+
+DS18x20::DS18x20(uint8_t eventId, 
+  gpio_num_t pin, onewire_addr_t address, int8_t index, DS18x20_RESOLUTION resolution, bool saveScratchPad,
+  const char* sensorName, const char* topicName, const bool topicLocal, 
+  const uint32_t minReadInterval, const uint16_t errorLimit,
+  cb_status_changed_t cb_status, cb_publish_data_t cb_publish)
+:rSensor(eventId, 1,
+  sensorName, topicName, topicLocal, 
+  minReadInterval, errorLimit,
+  cb_status, cb_publish)
+{
+  _pin = pin;
+  _index = index;
+  _address = address;
+  _resolution = resolution;
+  _saveScratchPad = saveScratchPad;
+  _model = MODEL_UNKNOWN;
+  _parasitePower = false;
+}
+
+void DS18x20::setSensorItems(rSensorItem* itemTemperature)
+{
+  setSensorItem(0, itemTemperature);
+}
+
+sensor_status_t DS18x20::sensorBusReset()
+{
+  // Start bus
+  SENSOR_ERR_CHECK(gpio_reset_pin(_pin), "Failed to reset 1-Wire bus: reset GPIO!");
+  SENSOR_ERR_CHECK(gpio_set_direction(_pin, GPIO_MODE_INPUT_OUTPUT_OD), "Failed to reset 1-Wire bus: set GPIO direction!");
+
+  // Found device
+  bool ret = false;
+  if (_address == ONEWIRE_NONE) {
+    ret = scanDevices();
+  } else {
+    ret = addressSelect();
+  };
+  if (ret) {
+    return SENSOR_STATUS_OK;
+  };
+  rlog_e(logTAG, "Failed to reset 1-Wire bus: device not found!");
+  return SENSOR_STATUS_ERROR;
+}
+
+sensor_status_t DS18x20::sensorReset()
+{
+  // Start device
+  sensor_status_t rslt = readPowerSupply();
+  if (rslt == SENSOR_STATUS_OK) {
+    readROM(nullptr);
+    rslt = setResolution(_resolution);
+  };
+  return rslt;
+}
+
+sensor_status_t DS18x20::readRawData()
+{
+  sensor_status_t rslt = SENSOR_STATUS_OK;
+  if (!_check_resolution(_resolution)) {
+    rslt = getResolution(nullptr);
+  };
+
+  if (rslt == SENSOR_STATUS_OK) {
+    rslt = startConvert();
+    if (rslt == SENSOR_STATUS_OK) {
+      rslt = waitForConversion();
+      if (rslt == SENSOR_STATUS_OK) {
+        float value = NAN;
+        rslt = readTemperature(&value);
+        if (rslt == SENSOR_STATUS_OK) {
+          rslt = setRawValue(0, value);
+        };
+      };
+    };
+  };
+  return rslt;
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------- Device select ----------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+bool DS18x20::addressSelect()
+{
+  bool result = false;
+  if (onewire_reset(_pin)) {
+    if (_address == ONEWIRE_NONE) {
+      // If there's only one device on the bus, we can skip sending the ROM code and instruct it directly
+      result = onewire_skip_rom(_pin);
+    } else {
+      // If there are multiple devices on the bus, a Match ROM command must be issued to address a specific slave
+      result = onewire_select(_pin, _address);
+      // On Chinese fakes sometimes there are failures
+      if (!result) {
+        vTaskDelay(1);
+        result = onewire_select(_pin, _address);
+      };
+    };
+    if (!result) {
+      rlog_e(logTAG, "Sensor [%s]: failed to select sensor", getName());
+    };
+  } else {
+    rlog_e(logTAG, "Sensor [%s]: failed to reset 1-Wire bus on GPIO %d", getName(), _pin);
+  };
+  return result;
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------- Device search ----------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+bool DS18x20::validFamily(uint8_t faminly_byte) 
+{
+	switch (faminly_byte) {
+    case MODEL_DS18S20:
+    case MODEL_DS18B20:
+    case MODEL_DS1822:
+    case MODEL_DS1825:
+    case MODEL_DS28EA00:
+      return true;
+    default:
+      return false;
+	};
+}
+
+bool DS18x20::validAddress(uint8_t* rom_code) 
+{
+	return (onewire_crc8(rom_code, 7) == rom_code[7]);
+}
+
+bool DS18x20::readROM(uint64_t *rom_code)
+{
+  if (addressSelect() && onewire_write(_pin, DS18x20_READROM)) {
+    uint8_t buf[8] = {0};
+    for (int i = 0; i < 8; i++) {
+      buf[i] = onewire_read(_pin);
+    };
+    char data[17] = {0};
+    _ui64toa((uint64_t)buf, &data[0], 16);
+    rlog_d(logTAG, "Sensor [%s]: read ROM data: %s", getName(), data);
+    if (rom_code) {
+      *rom_code = (uint64_t)buf;
+    };
+    return true;
+  };
+  return false;
+}
+
+bool DS18x20::scanDevices()
+{
+  rlog_i(logTAG, "Search devices...");
+
+  uint16_t devicesCount = 0;
+  uint16_t ds18Count = 0;
+  _model = MODEL_UNKNOWN;
+  _address = ONEWIRE_NONE;
+
+  if (onewire_reset(_pin)) {
+    onewire_search_t search;
+    onewire_search_start(&search);
+    onewire_addr_t scan_address = ONEWIRE_NONE;
+    while ((scan_address = onewire_search_next(&search, _pin)) != ONEWIRE_NONE) {
+      devicesCount++;
+      if (validAddress((uint8_t*)&scan_address) && validFamily((uint8_t)scan_address)) {
+        ds18Count++;
+        char addr[17] = {0};
+        _ui64toa(scan_address, &addr[0], 16);
+        rlog_i(logTAG, "Found device #%d : %s", ds18Count, addr);
+        if ((_index == ds18Count) || ((_index == 0) && (ds18Count == 1))) {
+          _model = (DS18x20_MODEL)scan_address;
+          _address = scan_address;
+        };
+      };
+    };
+    if (devicesCount == 1) {
+      _address = ONEWIRE_NONE;
+    };
+    rlog_i(logTAG, "Found %d device%s", ds18Count, ds18Count == 1 ? "" : "s");    
+    return (ds18Count > 0) && validFamily((uint8_t)_model);
+  } else {
+    rlog_e(logTAG, "Sensor [%s]: failed to reset 1-Wire bus on GPIO %d", getName(), _pin);
+    return false;
+  };
+}
+
+#if CONFIG_SENSOR_AS_JSON
+
+char* DS18x20::jsonCustomValues()
+{
+  char addr[17] = {0};
+  _ui64toa(_address, &addr[0], 16);
+  return malloc_stringf("\"address\":\"%s\"", addr);
+}
+
+#endif // CONFIG_SENSOR_AS_JSON
+// -----------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------- Parasite power ----------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+sensor_status_t DS18x20::readPowerSupply()
+{
+  if (addressSelect()) {
+    if (onewire_write(_pin, ds18x20_READ_PWRSUPPLY)) {
+      int power = onewire_read_bit(_pin);
+      if (power >= 0) {
+        _parasitePower = power == 0;
+        if (_parasitePower) {
+          rlog_i(logTAG, "Parasite power detected for GPIO=%d", _pin);
+        } else {
+          rlog_i(logTAG, "Normal power detected for GPIO=%d", _pin);
+        };
+        return SENSOR_STATUS_OK;
+      };
+    };
+    rlog_e(logTAG, "Sensor [%s]: failed to read power supply mode", getName());
+  };
+  return SENSOR_STATUS_CONN_ERROR;
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------- ScratchPad ------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+sensor_status_t DS18x20::readScratchpad(uint8_t *buffer)
+{
+  if (addressSelect()) {
+    if (onewire_write(_pin, DS18x20_SCRATCHPAD_READ)) {
+      uint8_t crc;
+      uint8_t expected_crc; 
+      for (int i = 0; i < 8; i++)
+        buffer[i] = onewire_read(_pin);
+      crc = onewire_read(_pin);
+      expected_crc = onewire_crc8(buffer, 8);     
+      if (crc == expected_crc) {
+        return SENSOR_STATUS_OK;
+      } else {
+        rlog_e(logTAG, "Sensor [%s]: failed to read scratchpad: CRC failed "
+          "(temp: %02X %02X, alarm: %02X %02X, config: %02X, reserved: %02X %02X %02X, crc: %02X (expected: %02X))", 
+          getName(), buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6], buffer[7], crc, expected_crc);
+        return SENSOR_STATUS_CONN_ERROR;
+      };
+    };
+    rlog_e(logTAG, "Sensor [%s]: failed to read scratchpad", getName());
+  };
+  return SENSOR_STATUS_CONN_ERROR;
+}
+
+sensor_status_t DS18x20::writeScratchpad(uint8_t *buffer)
+{
+  if (addressSelect()) {
+    if (onewire_write(_pin, DS18x20_SCRATCHPAD_WRITE)) {
+      for (int i = 0; i < 3; i++) {
+        if (!onewire_write(_pin, buffer[i+SP_HIGH_ALARM_TEMP])) {
+          rlog_e(logTAG, "Sensor [%s]: failed to write scratchpad", getName());
+          return SENSOR_STATUS_CONN_ERROR;
+        };
+      };
+      // Autosave scratchpad
+      if (_saveScratchPad) {
+        return saveScratchpad();
+      } else {
+        return SENSOR_STATUS_OK;
+      };
+    };
+    rlog_e(logTAG, "Sensor [%s]: failed to write scratchpad", getName());
+  };
+  return SENSOR_STATUS_CONN_ERROR;
+}
+
+sensor_status_t DS18x20::saveScratchpad() 
+{
+  if (addressSelect()) {
+    if (onewire_write(_pin, DS18x20_SCRATCHPAD_COPY)) {
+      // For parasitic devices, power must be applied within 20us after issuing the convert command
+      // Specification: NV Write Cycle Time is typically 2ms, max 10ms
+      // Waiting 20ms to allow for sensors that take longer in practice
+      if (_parasitePower) onewire_power(_pin);
+      vTaskDelay(pdMS_TO_TICKS(20));
+      if (_parasitePower) onewire_depower(_pin); 
+      return SENSOR_STATUS_OK;
+    };
+    rlog_e(logTAG, "Sensor [%s]: failed to copy scratchpad", getName());
+  };
+  return SENSOR_STATUS_CONN_ERROR;
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------ Resolution -----------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+sensor_status_t DS18x20::getResolution(DS18x20_RESOLUTION *resolution) 
+{
+  // DS1820 and DS18S20 have no resolution configuration register
+	if (_model == MODEL_DS18S20) {
+    _resolution = DS18x20_RESOLUTION_12_BIT;
+    if (resolution) *resolution = _resolution;
+		return SENSOR_STATUS_NOT_SUPPORTED;
+  };
+
+	_resolution = DS18x20_RESOLUTION_INVALID;
+  uint8_t scratchpad[9];
+  sensor_status_t rslt = readScratchpad(scratchpad);
+	if (rslt == SENSOR_STATUS_OK) {
+    _resolution = (DS18x20_RESOLUTION)(((scratchpad[SP_CONFIGURATION] >> 5) & 0x03) + (uint8_t)DS18x20_RESOLUTION_9_BIT);
+    if (!_check_resolution(_resolution)) {
+      _resolution = DS18x20_RESOLUTION_INVALID;
+      rlog_e(logTAG, "Sensor [%s]: invalid resolution read from device: 0x%02x", getName(), scratchpad[SP_CONFIGURATION]);
+    };
+    if (resolution) *resolution = _resolution;
+	};
+	return rslt;
+}
+
+sensor_status_t DS18x20::setResolution(DS18x20_RESOLUTION resolution)
+{
+  // DS1820 and DS18S20 have no resolution configuration register
+  if (_model == MODEL_DS18S20) {
+    return SENSOR_STATUS_OK;
+  };
+  if (!_check_resolution(resolution)) {
+    rlog_e(logTAG, "Sensor [%s]: unsupported resolution %d", getName(), resolution);
+    return SENSOR_STATUS_NOT_SUPPORTED;
+  };
+
+  // Read scratchpad up to and including configuration register
+  uint8_t scratchpad[9];
+  sensor_status_t rslt = readScratchpad(scratchpad);
+  if (rslt == SENSOR_STATUS_OK) {
+    // Check current resolution
+    _resolution = (DS18x20_RESOLUTION)(((scratchpad[SP_CONFIGURATION] >> 5) & 0x03) + (uint8_t)DS18x20_RESOLUTION_9_BIT);
+    if (_check_resolution(_resolution) && (_resolution == resolution)) {
+      rlog_i(logTAG, "Sensor [%s]: resolution already set to %d bits", getName(), (int)_resolution);
+      return SENSOR_STATUS_OK;
+    };
+    // Modify configuration register to set resolution
+    scratchpad[SP_CONFIGURATION] = ((((uint8_t)resolution - 1) & 0x03) << 5) | 0x1f;
+    // Write bytes 2, 3 and 4 of scratchpad
+    rslt = writeScratchpad(scratchpad);
+    if (rslt == SENSOR_STATUS_OK) {
+      // Verify changes
+      rslt = getResolution(nullptr);
+      if (rslt == SENSOR_STATUS_OK) {
+        if (_resolution == resolution) {
+          rlog_i(logTAG, "Sensor [%s]: resolution set to %d bits", getName(), (int)_resolution);
+          return SENSOR_STATUS_OK;
+        } else {
+          // Resolution change failed - update the info resolution with the value read from configuration
+          rlog_w(logTAG, "Sensor [%s]: resolution consistency lost - refreshed from device: %d", getName(), (int)_resolution);
+          return SENSOR_STATUS_ERROR;
+        };
+      };
+    };
+  };
+  return rslt;
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------- Delays --------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+void DS18x20::waitForDuration()
+{
+  switch (_resolution) {
+    case DS18x20_RESOLUTION_9_BIT:
+      vTaskDelay(CONVERSION_TIMEOUT_9_BIT / portTICK_PERIOD_MS);
+      break;
+    case DS18x20_RESOLUTION_10_BIT:
+      vTaskDelay(CONVERSION_TIMEOUT_10_BIT / portTICK_PERIOD_MS);
+      break;
+    case DS18x20_RESOLUTION_11_BIT:
+      vTaskDelay(CONVERSION_TIMEOUT_11_BIT / portTICK_PERIOD_MS);
+      break;
+    default:
+      vTaskDelay(CONVERSION_TIMEOUT_12_BIT / portTICK_PERIOD_MS);
+      break;
+  };
+}
+
+sensor_status_t DS18x20::waitForDeviceSignal()
+{
+  // wait for conversion to complete - all devices will pull bus low once complete
+  TickType_t max_conversion_ticks = CONVERSION_TIMEOUT_12_BIT / portTICK_PERIOD_MS;
+  TickType_t start_ticks = xTaskGetTickCount();
+  TickType_t duration_ticks = 0;
+  uint8_t status = 0;
+  do {
+    vTaskDelay(1);
+    status = onewire_read_bit(_pin);
+    duration_ticks = xTaskGetTickCount() - start_ticks;
+  } while (status == 0 && duration_ticks < max_conversion_ticks);
+
+  if (duration_ticks >= max_conversion_ticks) {
+    rlog_v(logTAG, "Sensor [%s]: conversion timed out", getName());
+    return SENSOR_STATUS_CONN_ERROR;
+  };
+
+  return SENSOR_STATUS_OK;
+}
+
+sensor_status_t DS18x20::waitForConversion()
+{
+  if (_parasitePower) {
+    if (onewire_power(_pin)) {
+      waitForDuration();
+      onewire_depower(_pin);
+      return SENSOR_STATUS_OK;
+    };
+    return SENSOR_STATUS_CONN_ERROR;
+  } else {
+    return waitForDeviceSignal();
+  };
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------- Read temperature --------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+sensor_status_t DS18x20::startConvert()
+{
+  if (addressSelect()) {
+    // initiate a temperature measurement
+    if (onewire_write(_pin, DS18x20_TEMP_CONVERT)) {
+      return SENSOR_STATUS_OK;
+    };
+    rlog_e(logTAG, "Sensor [%s]: failed to initiate a temperature measurement", getName());
+  };
+  return SENSOR_STATUS_CONN_ERROR;
+}
+
+sensor_status_t DS18x20::readTemperature(float * value)
+{
+  if (addressSelect()) {
+    uint8_t scratchpad[9];
+    if (readScratchpad(scratchpad) == SENSOR_STATUS_OK) {
+      // https://github.com/cpetrich/counterfeit_DS18x20#solution-to-the-85-c-problem
+      if (scratchpad[SP_COUNT_REMAIN] == 0x0c && scratchpad[SP_TEMP_MSB] == 0x05 && scratchpad[SP_TEMP_LSB] == 0x50) {
+        rlog_e(logTAG, "Sensor [%s]: read power-on value (85.0)", getName());
+        return SENSOR_STATUS_ERROR;
+      };
+
+      rlog_v(logTAG, "Read data: %02X %02X, resolution %02X (%d)", scratchpad[SP_TEMP_LSB], scratchpad[SP_TEMP_MSB], scratchpad[SP_CONFIGURATION], _resolution);
+      _resolution = (DS18x20_RESOLUTION)(((scratchpad[SP_CONFIGURATION] >> 5) & 0x03) + (uint8_t)DS18x20_RESOLUTION_9_BIT);
+      int16_t temp_raw = (((int16_t)scratchpad[SP_TEMP_MSB]) << 11) | (((int16_t)scratchpad[SP_TEMP_LSB]) << 3);
+      /*
+      DS1820 and DS18S20 have a 9-bit temperature register.
+      Resolutions greater than 9-bit can be calculated using the data from
+      the temperature, and COUNT REMAIN and COUNT PER °C registers in the
+      scratchpad.  The resolution of the calculation depends on the model.
+      While the COUNT PER °C register is hard-wired to 16 (10h) in a
+      DS18S20, it changes with temperature in DS1820.
+      After reading the scratchpad, the TEMP_READ value is obtained by
+      truncating the 0.5°C bit (bit 0) from the temperature data. The
+      extended resolution temperature can then be calculated using the
+      following equation:
+                                        COUNT_PER_C - COUNT_REMAIN
+      TEMPERATURE = TEMP_READ - 0.25 + --------------------------
+                                              COUNT_PER_C
+      Hagai Shatz simplified this to integer arithmetic for a 12 bits
+      value for a DS18S20, and James Cameron added legacy DS1820 support.
+      See - http://myarduinotoy.blogspot.co.uk/2013/02/12bit-result-from-ds18s20.html
+      */
+      if ((_model == MODEL_DS18S20) && (scratchpad[SP_COUNT_PER_C] != 0)) {
+        temp_raw = ((temp_raw & 0xfff0) << 3) - 32 + (((scratchpad[SP_COUNT_PER_C] - scratchpad[SP_COUNT_REMAIN]) << 7) / scratchpad[SP_COUNT_PER_C]);
+      };
+
+      if (value) {
+        // Convert from raw to Celsius: C = RAW/128
+        *value = (float)temp_raw * 0.0078125f; 
+      };
+      return SENSOR_STATUS_OK;
+    };
+  };
+  return SENSOR_STATUS_CONN_ERROR;
+}
+
+sensor_value_t DS18x20::getTemperature(const bool readSensor)
+{
+  return getItemValue(0, readSensor);
+}
